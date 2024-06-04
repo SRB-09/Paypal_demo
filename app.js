3bb125575f16672979a4e6db3ea14cb7fe0e2915
Suraj Raj Ranabhat <147985897+SRB-09@users.noreply.github.com>
2024-06-04 13:45:53 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-// const videoPlayer = document.querySelector(".first-section");
-const controls = document.querySelector(".controls");
-const video = document.querySelector(".video");
-// const icon = document.querySelector(".icon");
-const iconPause = '<i class="fa-sharp fa-solid fa-pause" style="color: #ffffff;"></i>';
-const iconPlay = '<i class="fa-solid fa-play" style="color: #ffffff;"></i>';
-
-controls.addEventListener("click", function(event){
-    event.preventDefault();
-    if(this.innerHTML === '<i class="fa-sharp fa-solid fa-pause" style="color: #ffffff;"></i>'){
-        video.pause();
-        this.innerHTML = iconPlay;
-        document.querySelector(".overlay").style.backgroundColor = "black";
-        document.querySelector(".overlay").style.opacity = "0.3";
-        document.querySelector(".overlay").style.transition = "all  ease-out";
-        document.querySelector(".overlay").style.zIndex = "-2";
-        document.querySelector(".controls").style.zIndex = "1";
-    }else{
-        video.play();
-        this.innerHTML = iconPause;
-        document.querySelector(".overlay").style.transition = "all  ease-out";
-        document.querySelector(".overlay").style.opacity = "0";
-        document.querySelector(".overlay").style.zIndex = "-2";
-    }
-});
-
 document.getElementById("buy").addEventListener("mouseover", function(){
     document.querySelector(".buy-list").style.transform = "translateY(1px)";
     document.querySelector(".buy-list").style.transition = "all 0.5s ease-out";
@@ -59,4 +33,28 @@ document.getElementById("buy-close").addEventListener("click", (e) => {
     document.querySelector(".pay-list").style.transition = "0.5s all ease-in";
 });
 
+// const videoPlayer = document.querySelector(".first-section");
+const controls = document.querySelector(".controls");
+const video = document.querySelector(".video");
+// const icon = document.querySelector(".icon");
+const iconPause = '<i class="fa-sharp fa-solid fa-pause" style="color: #ffffff;"></i>';
+const iconPlay = '<i class="fa-solid fa-play" style="color: #ffffff;"></i>';
 
+controls.addEventListener("click", function(event){
+    event.preventDefault();
+    if(this.innerHTML === '<i class="fa-sharp fa-solid fa-pause" style="color: #ffffff;"></i>'){
+        video.pause();
+        this.innerHTML = iconPlay;
+        document.querySelector(".overlay").style.backgroundColor = "black";
+        document.querySelector(".overlay").style.opacity = "0.3";
+        document.querySelector(".overlay").style.transition = "all  ease-out";
+        document.querySelector(".overlay").style.zIndex = "-2";
+        document.querySelector(".controls").style.zIndex = "1";
+    }else{
+        video.play();
+        this.innerHTML = iconPause;
+        document.querySelector(".overlay").style.transition = "all  ease-out";
+        document.querySelector(".overlay").style.opacity = "0";
+        document.querySelector(".overlay").style.zIndex = "-2";
+    }
+});
